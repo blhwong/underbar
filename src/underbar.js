587f40e588ce76052b53100216bfcb49db7941b4
Brandon Wong <b.lh.wong@gmail.com>
2017-01-16 23:37:23 -0600

    Pass all uniq test cases

--- src/underbar.js
+++ src/underbar.js
@@ -98,6 +98,13 @@
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var uniqueArray = [];
+    _.each(array, function(element) {
+      if (!uniqueArray.includes(element)) {
+        uniqueArray.push(element);
+      }
+    });
+    return uniqueArray;
   };
 
 
