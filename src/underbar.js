bc2d3de1ac7dffb36cbe214b177d2117dd469b4b
Brandon Wong <b.lh.wong@gmail.com>
2017-01-18 18:45:19 -0600

    Pass all test cases with extend using each instead of forloop

--- src/underbar.js
+++ src/underbar.js
@@ -230,11 +230,11 @@
   //     bla: "even more stuff"
   //   }); // obj1 now contains key1, key2, key3 and bla
   _.extend = function(obj) {
-    for (var index = 1; index < arguments.length; index++) {
-      for (var prop in arguments[index]) {
-        obj[prop] = arguments[index][prop];
-      }
-    }
+    _.each(Array.prototype.slice.call(arguments, 1), function(element) {
+      _.each(element, function(value, prop) {
+        obj[prop] = value;
+      });
+    });
     return obj;
   };
 
